443327e966d07eb814071db14299c4c250abbced
Mateusz <mateusz-wojcik@hotmail.com>
2023-01-22 21:16:11 +0100

    build fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,7 @@ class CustomDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
             rel="stylesheet"
