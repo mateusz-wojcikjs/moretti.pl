f73a93f11df822ee3e38a3aed4e13ecdfc85fc52
Mateusz <mateusz-wojcik@hotmail.com>
2023-01-31 22:31:53 +0100

    revert loading spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import Layout from "components/layout";
 import * as Utils from "utils";
 import { Product } from "interfaces/product.interface";
 import { Company } from "components/layout/Footer";
-import { useEffect, useState } from "react";
 
 interface CustomAppProps extends AppProps {
   links: string[];
@@ -15,38 +14,11 @@ interface CustomAppProps extends AppProps {
 
 function MyApp({ Component, pageProps, links, company }: CustomAppProps) {
   const { attributes } = company;
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const onPageLoad = () => {
-      setLoading(false);
-    };
-    // Check if the page has already loaded
-    if (document.readyState === "complete") {
-      onPageLoad();
-    } else {
-      window.addEventListener("load", onPageLoad);
-      // Remove the event listener when component unmounts
-      return () => window.removeEventListener("load", onPageLoad);
-    }
-  }, []);
-
-  if (!loading)
-    return (
-      <Layout links={links} company={attributes}>
-        <Component {...pageProps} />
-      </Layout>
-    );
 
   return (
-    <div className="lll">
-      <div id="loader">
-        <div className="loader-dot" />
-        <div className="loader-dot" />
-        <div className="loader-dot" />
-        <div className="loader-dot" />
-        <div className="loader-dot" />
-      </div>
-    </div>
+    <Layout links={links} company={attributes}>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
