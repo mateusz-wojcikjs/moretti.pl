42343881059e5b96055b6213c71c0a04b6ca6e2b
Mateusz <mateusz-wojcik@hotmail.com>
2024-05-15 22:36:18 +0200

    remove white space between navbar and background

--- components/layout/BodyBackground/BodyBackground.styled.ts
+++ components/layout/BodyBackground/BodyBackground.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledBodyBackground = styled.div`
   position: fixed;
   height: calc(100vh - 8rem);
-  margin-top: 10rem;
+  margin-top: 8rem;
   width: 100%;
   z-index: -1;
   opacity: 0.3;
